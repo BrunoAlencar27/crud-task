a43c93887053a6d364b29679d2fe3affe1fc5fba
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-09-13 09:47:40 -0300

    chore: configure prettier with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { prettier: eslintPluginPrettier },
+    extends: [js.configs.recommended, eslintConfigPrettier],
+    languageOptions: { globals: globals.node },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ]);
