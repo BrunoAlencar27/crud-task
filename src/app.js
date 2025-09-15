82c62dba00af256eb0146202e146be8edb981def
carlosdrt1 <carlosduarteinfo@gmail.com>
2025-09-15 13:40:59 -0300

    feat: add user repository, service and controller tasks

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { userRoutes } from './modules/user/userRoutes.js';
 
 export function createApp() {
   const app = express();
+
+  app.use(userRoutes);
 
   return app;
 }
---
+++ src/modules/user/userRespository.js
@@ -0,0 +1,5 @@
+export class UserRepository {
+  async findById(id) {
+    return 'user ' + id;
+  }
+}
