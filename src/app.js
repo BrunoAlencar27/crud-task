62a33588e09f974942238f74aedab83c3513adc8
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2025-09-20 11:52:45 -0300

    feat:connected mongoDB

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { userRoutes } from './modules/user/userRoutes.js';
+import { taskRoutes } from './modules/task/taskRoutes.js';
 
 export function createApp() {
   const app = express();
-
+  app.use(express.json());
+  app.use(taskRoutes);
   app.use(userRoutes);
 
   return app;
