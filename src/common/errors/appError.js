75cd22cded4f3f0a604ef116b391617cd615fdc7
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-09-22 20:35:29 -0300

    feat: add error handler to centralize error handling

---
+++ src/common/errors/appError.js
@@ -0,0 +1,10 @@
+export class AppError extends Error {
+  constructor(statusCode, messages) {
+    messages = Array.isArray(messages) ? messages : [messages];
+
+    super(messages.toString());
+
+    this.messages = messages;
+    this.statusCode = statusCode;
+  }
+}
