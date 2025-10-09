37b2440113b2a5b9ce1337b5fd843cde7e3f8e4e
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-10-09 20:13:31 -0300

    feat: add documentation for user and auth routes

--- src/modules/auth/authRoutes.js
+++ src/modules/auth/authRoutes.js
@@ -11,12 +11,148 @@ const userService = new UserService(userRepository);
 const authService = new AuthService(userService);
 const authController = new AuthController(authService);
 
+/**
+ * @openapi
+ * /auth/register:
+ *   get:
+ *     summary: Register a new user in application
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                example: username
+ *              email:
+ *                type: string
+ *                example: user@email.com
+ *              password:
+ *                type: string
+ *                example: password
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                id:
+ *                  type: string
+ *                  example: 102ksd00awd02
+ *                name:
+ *                  type: string
+ *                  example: username
+ *                email:
+ *                  type: string
+ *                  example: user@email.com
+ *                createdAt:
+ *                  type: string
+ *                  format: date-time
+ *                updatedAt:
+ *                  type: string
+ *                  format: date-time
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     example: '"email" must be a valid email'
+ *       409:
+ *         description: Conflict
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     example: 'email already registered'
+ */
 authRoutes.post('/register', (req, res, next) =>
   authController.register(req, res, next),
 );
+
+/**
+ * @openapi
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: email@email.com
+ *               password:
+ *                 type: string
+ *                 example: 12345678
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: user logged in successfully
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     example: invalid credentials
+ */
 authRoutes.post('/login', (req, res, next) =>
   authController.login(req, res, next),
 );
+
+/**
+ * @openapi
+ * /auth/logout:
+ *   post:
+ *     summary: Logout the current user
+ *     tags:
+ *       - Auth
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: user logged out successfully
+ */
 authRoutes.post('/logout', (req, res, next) =>
   authController.logout(req, res, next),
 );
