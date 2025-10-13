1d2047d95fe05bcaebdb5a0b49c15b9b19856653
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2025-10-13 07:29:39 -0300

    feature:configuration cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cros';
+import dotenv from 'dotenv';
 import { userRoutes } from './modules/user/userRoutes.js';
 import { taskRoutes } from './modules/task/taskRoutes.js';
 import { errorHandler } from './common/middlewares/errorHandler.js';
@@ -8,8 +10,11 @@ import { authRoutes } from './modules/auth/authRoutes.js';
 import { authMiddleware } from './common/middlewares/authMiddleware.js';
 import { swaggerSpec } from './config/swagger.js';
 
+dotenv.config();
+
 export function createApp() {
   const app = express();
+  app.use = express(cors({ origin: process.env.CORS_ORIGIN || '*' }));
   app.use(express.json());
   app.use(cookieParser());
 
