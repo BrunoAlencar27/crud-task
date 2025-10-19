8756b2dd7c8963937eb13646c89facf23349be1e
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2025-10-19 11:20:11 -0300

    refactor:completed type from string to boolean

--- src/modules/task/taskModel.js
+++ src/modules/task/taskModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const taskSchema = new mongoose.Schema(
   {
     description: { type: String, required: true },
-    completed: { type: String, required: true },
+    completed: { type: Boolean, required: true },
     userId: { type: String, required: true },
   },
   { timestamps: true },
